509373aec1f0205bc91bd4fefe67220d39aa6036
Wenslous Egesa <wayneotemahegesa@gmail.com>
2023-03-06 18:03:40 +0300

    communication via the pigeon method channel - correct outpu to the screen and proper disconnect on user leaving the app

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MessageBoard(),
     );
   }
 }
@@ -39,14 +39,21 @@ class _MyHomePageState extends State<MyHomePage> {
   Future getChat() async {
     final List<Chat?> chats = await ChatApi().search('Killer');
     final newChats = List<Chat>.from(chats);
-    print('${chats[0]!.message}');
+    print(
+        '${chats[0]!.message} #################################################');
     print('${chats[0]!.clients}');
   }
 
   void startTimer() {
-    Timer.periodic(const Duration(seconds: 3), (timer) {
+    Timer.periodic(const Duration(seconds: 1), (timer) {
       getChat();
     });
+  }
+
+  @override
+  void dispose() {
+    print('disposed');
+    super.dispose();
   }
 
   @override
@@ -62,24 +69,98 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: const Center(
+        child: MessageBoard(),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: getChat,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class Message {
+  final String author;
+  final String text;
+
+  Message({required this.author, required this.text});
+}
+
+class MessageBoard extends StatefulWidget {
+  const MessageBoard({Key? key}) : super(key: key);
+
+  @override
+  _MessageBoardState createState() => _MessageBoardState();
+}
+
+class _MessageBoardState extends State<MessageBoard> {
+  final List<Message> _messages = [];
+
+  final _authorController = TextEditingController();
+  final _textController = TextEditingController();
+
+  @override
+  void dispose() {
+    _authorController.dispose();
+    _textController.dispose();
+    super.dispose();
+  }
+
+  void _addMessage() {
+    final author = _authorController.text;
+    final text = _textController.text;
+
+    if (author.isNotEmpty && text.isNotEmpty) {
+      final message = Message(author: author, text: text);
+      setState(() {
+        _messages.add(message);
+      });
+      _authorController.clear();
+      _textController.clear();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Message Board'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: _messages.length,
+              itemBuilder: (context, index) {
+                final message = _messages[index];
+                return ListTile(
+                  title: Text(message.author),
+                  subtitle: Text(message.text),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                TextField(
+                  controller: _authorController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your name',
+                  ),
+                ),
+                TextField(
+                  controller: _textController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter your message',
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: _addMessage,
+                  child: const Text('Send'),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
